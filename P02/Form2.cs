d003b7fe2deba1f166be4753e44b52951de26873
Karel <pavlonk.04@spst.eu>
2022-02-09 09:04:23 +0100

    mocnina

---
+++ P02/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P02
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int n = Convert.ToInt32(textBox2.Text);
+            int mocnina = 1;
+            for (int i = 0; i < n; i++)
+            {
+                mocnina *= a;
+            }
+            label1.Text = mocnina.ToString();
+        }
+    }
+}
