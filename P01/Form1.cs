4efeec22e67d836bfaa87856d945c65e3d94303f
Karel <pavlonk.04@spst.eu>
2022-02-09 08:58:37 +0100

    Přidejte soubory projektu.

---
+++ P01/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(textBox1.Text);
+            int faktorial = 1;
+            for (int i = 2; i <= value; i++)
+            {
+                faktorial *= i;
+            }
+            label1.Text = faktorial.ToString();
+        }
+    }
+}
